2cafd930942546925c7bd1af78b93a8af800693c
Nishu singh <nishu.frontend@gmail.com>
2026-04-13 09:42:33 +0530

    Render the data with map method

--- p-2-resuable-profile-card/src/App.jsx
+++ p-2-resuable-profile-card/src/App.jsx
@@ -2,9 +2,36 @@ import Card from "./components/Card";
 import "./App.css";
 
 function App() {
+  const profiles = [
+    {
+      id: 1,
+      name: "Mark",
+      title: "Front-End developer",
+      bio: "I like to work with different front-end technologies and play video games.",
+    },
+    {
+      id: 2,
+      name: "Tiffany",
+      title: "Engineering manager",
+      bio: "I have worked in tech for 15 years and love to help people grow in this industry.",
+    },
+    {
+      id: 3,
+      name: "Doug",
+      title: "Back-End developer",
+      bio: "I have been a software developer for over 20 years and I love working with Go and Rust.",
+    },
+  ];
   return (
     <div className="flex-container">
-      <Card name="Nishu" title="Front-End developer" bio="Responsive Degian" />
+      {profiles.map((profile) => (
+        <Card
+          key={profile.id}
+          name={profile.name}
+          title={profile.title}
+          bio={profile.bio}
+        />
+      ))}
     </div>
   );
 }
